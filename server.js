44c4c8394cbb74b87c6061330487b11ff70b3385
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-05-01 23:21:40 +0530

    unexport all pins first
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
  * Created by rishabhkhanna on 01/05/17.
  */
 const gpio = require('rpi-gpio');
-
+gpio.destroy(function () {
+    console.log("all pins unexported");
+});
 gpio.setup(17, gpio.DIR_OUT,write);
 
 function write() {
-    gpio.write(17 , true , function (err) {
-        if(err) throw err;
+    gpio.write(17 , true , function () {
         console.log("written to pin");
     })
 }
